d6d4509b079f0af79c33d7f35a8d35664d944bf2
xiaohuli <waterdoor@foxmail.com>
2021-04-22 22:28:35 +0800

    feat: add test (#29)
    * feat: add test
    
    * feat: test

---
+++ fwef/add.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2021-04-21 08:38:44
+ * @LastEditTime: 2021-04-21 08:41:30
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /Doter/project/entry/test.ts
+ */
+export function sum(a: number, b: number) {
+    return a + b;
+}
+// module.exports = sum;
